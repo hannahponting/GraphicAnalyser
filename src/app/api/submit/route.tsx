d6ffc615a61abb0c95469eb48f416b0803b094bb
Hannah Ponting <hannahponting01@gmail.com>
2025-04-05 22:27:08 +0100

    fix first draft

--- src/app/api/submit/route.tsx
+++ src/app/api/submit/route.tsx
@@ -1,13 +1,13 @@
 import sendRequest from "@/utils/sendRequest";
 
-export async function POST(req) {
+export async function POST(req : Request) {
     try {
         const body = await req.json();
         const response = await sendRequest(body.b64String)
         return Response.json({ response: response });
 
-    } catch (err) {
-      return Response.json({ success: false, error: err.message }, { status: 500 });
+    } catch {
+      return Response.json({ success: false, error: 'error' }, { status: 500 });
     }
   }
   
